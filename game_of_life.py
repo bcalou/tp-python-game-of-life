febe9e04ea73f7acfb0799d378194b04fa2da7f0
Likarios <likarios.lebeks@gmail.com>
2021-12-16 10:41:25 +0100

    Fixed game entirely and made drawing size dynamic

--- game_of_life.py
+++ game_of_life.py
@@ -7,14 +7,15 @@ class Image:
     surface: pygame.surface.Surface
     rect: pygame.rect.Rect
 
-    def __init__(self, texture: str, screen: pygame.surface.Surface):
+    def __init__(self, texture: str, size_x: int, size_y: int, screen: pygame.surface.Surface):
         self.surface = pygame.image.load(texture)
+        self.surface = pygame.transform.scale(self.surface, (size_x, size_y))
         self.rect = self.surface.get_rect()
         self.screen = screen
 
     def set_position(self, x: int, y: int):
         self.rect.topleft = (y, x)
-
+        
     def paint(self, x: int = 0, y: int = 0):
         self.set_position(x, y)
         self.screen.blit(self.surface, self.rect)
@@ -22,60 +23,52 @@ class Image:
 # Go through a state and draw every tile based on its number
 def print_state(state: list[list[int]], screen: pygame.surface.Surface):
         screen.fill((0, 0, 0))
+        size_per_tile = screen.get_size()[0] // len(state)
         for i in range(len(state)):
             for j in range(len(state[i])):
                 if state[i][j] == 0:
-                    dead: Image = Image("dead_50.png", screen)
-                    dead.paint(i * 50, j * 50)
+                    dead: Image = Image("dead.png", size_per_tile, size_per_tile, screen)
+                    dead.paint(i * size_per_tile, j * size_per_tile)
                 else:
-                    alive: Image = Image("alive_50.png", screen)
-                    alive.paint(i * 50, j * 50)
+                    alive: Image = Image("alive.png", size_per_tile, size_per_tile, screen)
+                    alive.paint(i * size_per_tile, j * size_per_tile)
 
 # Go through a state and make avery number evolve depending on its surroundings
 def get_next_state(state: list[list[int]]) -> list[list[int]]:
     next_state: list[list[int]] = copy.deepcopy(state)
     for i in range(len(state)):
         for j in range(len(state[i])):
-            # Count the number of alive tiles arround the current tile
-            alive_tiles: int = 0
-            if i > 0:
-                if j > 0:
-                    if state[i-1][j-1] > 0:
-                        alive_tiles += 1
-                if state[i-1][j] > 0:
-                    alive_tiles += 1
-                if j < len(state[i-1]) - 1:
-                    if state[i-1][j+1] > 0:
-                        alive_tiles += 1
-            if j > 0:
-                if state[i][j-1] > 0:
-                    alive_tiles += 1
-            if j < len(state[i]) - 1:
-                if state[i][j+1] > 0:
-                    alive_tiles += 1
-            if i < len(state) - 1:
-                if j > 0:
-                    if state[i+1][j-1] > 0:
-                        alive_tiles += 1
-                if state[i+1][j] > 0:
-                    alive_tiles += 1
-                if j < len(state[i+1]) - 1:
-                    if state[i+1][j+1] > 0:
-                        alive_tiles += 1
+            alive_tiles = count_alive_tiles(state, i, j)
             # The two rules of the game of life
-            if(i == 0 and j == 2):
-                print("Alive tiles for (" + str(i) + ", " + str(j) + "): " + str(alive_tiles))
-            if alive_tiles == 3 and state[i][j] == 0:
-                next_state[i][j] = 1
-            elif state[i][j] > 0:
+            if state[j][i] == 0 and alive_tiles == 3:
+                # This tile borns
+                next_state[j][i] = 1
+            elif state[j][i] > 0:
                 if not (alive_tiles == 2 or alive_tiles == 3):
-                    state[i][j] = 0
+                    # This tile dies
+                    next_state[j][i] = 0
     return next_state
+
+# Count the number of alive tiles arround a specific tile
+def count_alive_tiles(state: list[list[int]], x: int, y: int) -> int:
+    alive_tiles: int = 0
+    for i in range(x - 1, x + 2):
+        for j in range(y - 1, y + 2):
+            check: bool = True
+            # The current tile is not being tested
+            if i == x and j == y:
+                check = False
+            # If a tile is outside the boundary, it is not tested
+            if i == -1 or i == len(state) or j == -1 or j == len(state):
+                check = False
+            # If the tested tile is alive, increase the counter
+            if check and state[j][i] > 0:
+                alive_tiles += 1
+    return alive_tiles
 
 # The main game function
 def game(initial_state: list[list[int]]):
     pygame.init()
-
     width: int = 1000
     height: int = 1000
     screen: pygame.surface.Surface = pygame.display.set_mode((width, height))
@@ -94,37 +87,47 @@ def game(initial_state: list[list[int]]):
                 done = True
 
         # Compute the next state based on the initial state
-        current_state = get_next_state(initial_state)
+        current_state = get_next_state(current_state)
 
         # Draw the current state
         print_state(current_state, screen)
         pygame.display.flip()
-        clock.tick(1)
+        clock.tick(15)
 
     # Close the window when the loop is over
     pygame.quit()
 
 # Main thread
 initial_state: list[list[int]] = [
-    [0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0],
+    [0, 0, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 0, 0, 0, 0, 0, 0, 1, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 0],
+    [0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0],
+    [0, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
 ]
 game(initial_state)
