d7ba63bda3f11d94ae1cc0f3219485d6caf76416
Victor Mommalier <victormommalier1@gmail.com>
2022-01-13 00:20:26 +0100

    my game of life

---
+++ game_of_life.py
@@ -0,0 +1,55 @@
+import pygame
+from copy import deepcopy
+from display import *
+
+def count_neighbors(current_state: list[list[int]], x: int, y: int) -> int:
+    return sum([1 for i in (-1, 0, 1) for j in (-1, 0, 1)
+                if x+i >= 0 and x+i < len(current_state[0]) and
+                y+j >= 0 and y+j < len(current_state) and
+                (i != 0 or j != 0) and
+                current_state[y+j][x+i] == 1])
+
+def get_next_state(current_state: list[list[int]]) -> list[list[int]]:
+    # Copy list
+    next_state: list[list[int]] = deepcopy(current_state)
+    for y in range(len(current_state)):
+        for x in range(len(current_state[y])):
+            # For every cell, check neighbors count
+            neighborCellsCount: int = count_neighbors(current_state, x, y)
+
+            # Apply rules
+            if neighborCellsCount < 2 or neighborCellsCount > 3:
+                next_state[y][x] = 0
+            elif neighborCellsCount == 3:
+                next_state[y][x] = 1
+    return next_state
+
+
+# Init
+pygame.init()
+current_state: list[list[int]] = deepcopy(initial_state)
+screen: pygame.surface.Surface = pygame.display.set_mode(SCREEN_SIZE)
+clock = pygame.time.Clock()
+
+# Loop
+playing: bool = False
+done: bool = False
+while not done:
+    for event in pygame.event.get():
+        # Close button
+        if event.type == pygame.QUIT:
+            done = True
+        elif event.type == pygame.KEYDOWN:
+            # Escape = Quit
+            if event.key == pygame.K_ESCAPE:
+                done = True
+            # Start / Pause simulation
+            elif event.key == pygame.K_SPACE:
+                playing = not playing
+    if playing:
+        current_state = get_next_state(current_state)
+    update_screen(screen, current_state)
+    clock.tick(FRAME_RATE)
+
+pygame.quit()
+
