bf734233a47ff49577c646dcd2bff62814118da2
Hippo <hippolyte.guillemeteau1@uqac.ca>
2022-01-13 03:16:58 +0100

    fin mise en forme

--- game_of_life/game.py
+++ game_of_life/game.py
@@ -1,20 +1,22 @@
 import copy
 
+
 def count_neighbors(current_state: list[list[int]], x: int, y: int) -> int:
-    count : int = 0
-    for i in (-1, 0, 1): 
-        for j in (-1, 0, 1):
-            if x+i >= 0 and x+i < len(current_state[0]) and y+j >= 0 and y+j < len(current_state) and (i != 0 or j != 0) and current_state[y+j][x+i] == 1 :
+    count: int = 0
+    for col in (-1, 0, 1):
+        for line in (-1, 0, 1):
+            if x+col >= 0 and x+col < len(current_state[0]) and y+line >= 0 and y+line < len(current_state) and (col != 0 or line != 0) and current_state[y+line][x+col] == 1:
                 count += 1
     return count
 
+
 def get_next_state(current_state: list[list[int]]) -> list[list[int]]:
     next_state: list[list[int]] = copy.deepcopy(current_state)
-    for y in range(len(current_state)):
-        for x in range(len(current_state[y])):
-            cellsCount: int = count_neighbors(current_state, x, y)
+    for i in range(len(current_state)):
+        for y in range(len(current_state[i])):
+            cellsCount: int = count_neighbors(current_state, y, i)
             if cellsCount < 2 or cellsCount > 3:
-                next_state[y][x] = 0
+                next_state[i][y] = 0
             elif cellsCount == 3:
-                next_state[y][x] = 1
+                next_state[i][y] = 1
     return next_state
