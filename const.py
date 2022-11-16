aaf3d8cff14462a175817119ae6bd368795b28cf
Antoine <antoine.martin885@gmail.com>
2022-11-16 15:01:26 +0100

    Add step 1

---
+++ const.py
@@ -0,0 +1,2 @@
+SCREEN_SIZE: (int, int) = (500, 500)
+FRAME_RATE: int = 10
--- game_of_life.py
+++ game_of_life.py
@@ -1,7 +1,29 @@
 import pygame
 
+from const import SCREEN_SIZE, FRAME_RATE
+
 pygame.init()
 
+screen: pygame.surface.Surface = pygame.display.set_mode(SCREEN_SIZE)
+
+clock = pygame.time.Clock()
+
+done: bool = False
+
+# While the game is not over
+while not done:
+
+    # Listen for all events
+    for event in pygame.event.get():
+
+        # Quit the infinite loop when the user presses the close button
+        if event.type == pygame.QUIT:
+            done = True
+
+    print("Update !")
+    clock.tick(FRAME_RATE)
+
+pygame.quit()
 
 def get_next_state():
     pass
