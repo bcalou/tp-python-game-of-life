b5e1d28f069d6b3b57491004245779abeccc7e37
JameFLC <jame.flochlecarour@gmail.com>
2022-11-16 16:23:46 +0100

    Upgades and reflections

--- game_of_life.py
+++ game_of_life.py
@@ -2,7 +2,7 @@ import pygame
 import copy
 
 
-initial_state: list[list[int]] = [
+current_matrix_world: list[list[int]] = [
     [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
         0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
     [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
@@ -103,38 +103,61 @@ initial_state: list[list[int]] = [
         0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
 ]
 
+"""
+TODO : Actuellement l'algo crée une copie du la matrice actuelle et fait les oppérations dans cette copie
 
-def get_next_state(matrix: list[list[int]]):
-    new_matrix: list[list[int]] = copy.deepcopy(matrix)
-    for y in range(len(matrix)):
-        for x in range(len(matrix[0])):
-            total: int = get_adjacent_total_value(matrix, x, y)
+Autres aroches: 
+    -- Au lieu de faire une copie complette et de changer avec els coordonées, créer un tableau vide et le remplir au fur et à mesure, cela fera moins de copies 
+    -- Utiliser deux tableau comme un doule buffer à la openGL
+    -- PyCUDA ?
+"""
+
+
+def get_next_state(current_matrix: list[list[int]]):
+    """Copy the current matrix world into a new one then apply the conways game of life rules on each cells of hte new one, finish by returning the new matrix world"""
+    new_matrix: list[list[int]] = copy.deepcopy(current_matrix)
+
+    for current_cell_pos_y in range(len(current_matrix)):
+        for current_cell_pos_x in range(len(current_matrix[0])):
+
+            total: int = get_number_of_living_neighboring_cells(
+                current_matrix, current_cell_pos_x, current_cell_pos_y)
+
             if total == 3:
-                new_matrix[x][y] = 1
+                new_matrix[current_cell_pos_x][current_cell_pos_y] = 1
+
             if total > 3 or total < 2:
-                new_matrix[x][y] = 0
+                new_matrix[current_cell_pos_x][current_cell_pos_y] = 0
     return copy.deepcopy(new_matrix)
 
 
-def get_adjacent_total_value(matrix: list[list[int]], pos_x: int, pos_y: int) -> int:
+def get_number_of_living_neighboring_cells(matrix: list[list[int]], pos_x: int, pos_y: int) -> int:
+    """Calculate the number of alive neighboring from a specific cell"""
     total: int = 0
+    # Go thru the cells in the range one of the current cells
     for offset_x in range(-1, 2):
         for offset_y in range(-1, 2):
+            # Dont add the current cell value, only its neighboring cells values
             if offset_y == 0 and offset_x == 0:
                 continue
-            x = pos_x + offset_x
-            y = pos_y + offset_y
-            if (x < 0 or x >= len(matrix)) or (y < 0 or y >= len(matrix[0])):
+
+            # Current neighbor cell position
+            neighbor_pos_x = pos_x + offset_x
+            neighbor_pos_y = pos_y + offset_y
+
+            # Check if the neighboring cell exist
+            if (neighbor_pos_x < 0 or neighbor_pos_x >= len(matrix)) or (neighbor_pos_y < 0 or neighbor_pos_y >= len(matrix[0])):
                 continue
-            total += matrix[x][y]
+            # Add the current neighbour cell value
+            total += matrix[neighbor_pos_x][neighbor_pos_y]
     return total
 
 
-def draw_matrix(matrix: list[list[int]]):
+def draw_matrix(matrix: list[list[int]], screen: pygame.Surface):
+    """Function that draw a matrix of black or white rectangles depending on the value of the passed matrix"""
     screen_size_x, screen_size_y = pygame.display.get_window_size()
     cell_size_x = screen_size_x // len(matrix)
     cell_size_y = screen_size_y // len(matrix[0])
-
     for x in range(len(matrix)):
         for y in range(len(matrix[0])):
             pixel_value = 255 if matrix[x][y] == 1 else 0
@@ -142,26 +165,35 @@ def draw_matrix(matrix: list[list[int]]):
                              cell_size_y*y, cell_size_x*(x+1), cell_size_y*(y+1)))
 
 
-pygame.init()
+# Defining main function
+def main():
+    global current_matrix_world
+    pygame.init()
+    clock = pygame.time.Clock()
 
-clock = pygame.time.Clock()
+    done: bool = False
 
-done: bool = False
+    CELL_SIZE = 16
+    screen: pygame.surface.Surface = pygame.display.set_mode(
+        (len(current_matrix_world)*CELL_SIZE, len(current_matrix_world[0])*CELL_SIZE))
+    # While the game is not over
+    while not done:
+        screen.fill((0, 0, 0))
+        draw_matrix(current_matrix_world, screen)
+        current_matrix_world = get_next_state(current_matrix_world)
+        # Listen for all events
+        for event in pygame.event.get():
+            # Quit the infinite loop when the user presses the close button
+            if event.type == pygame.QUIT:
+                done = True
+        pygame.display.flip()
 
-CELL_SIZE = 16
-screen: pygame.surface.Surface = pygame.display.set_mode(
-    (len(initial_state)*CELL_SIZE, len(initial_state[0])*CELL_SIZE))
-# While the game is not over
-while not done:
-    screen.fill((0, 0, 0))
-    draw_matrix(initial_state)
-    initial_state = get_next_state(initial_state)
-    # Listen for all events
-    for event in pygame.event.get():
-        # Quit the infinite loop when the user presses the close button
-        if event.type == pygame.QUIT:
-            done = True
-    pygame.display.flip()
+    pygame.quit()
+
+    pass
 
 
-pygame.quit()
+# Using the special variable
+# __name__
+if __name__ == "__main__":
+    main()
