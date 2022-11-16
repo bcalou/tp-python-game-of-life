696aff7448ff5236d4860bd6aed067e4fc006c18
82L <16747176+82L@users.noreply.github.com>
2022-11-16 15:03:46 +0100

    Pygame integration

--- game_of_life.py
+++ game_of_life.py
@@ -1,2 +1,32 @@
+import pygame
+
+SCREEN_SIZE:int = 1000
+pygame.init()
+
+screen: pygame.surface.Surface = pygame.display.set_mode((SCREEN_SIZE, SCREEN_SIZE))
+
+clock = pygame.time.Clock()
+
+done: bool = False
+
+# While the game is not over
+while not done:
+    screen.fill((255, 255, 255))
+    # Listen for all events
+    for event in pygame.event.get():
+
+        # Quit the infinite loop when the user presses the close button
+        if event.type == pygame.QUIT:
+            done = True
+
+    pygame.draw.rect(screen, (0, 0, 0), (100, 200, 100, 100))
+
+
+    print("Update !")
+    pygame.display.flip()
+
+    clock.tick(1)
+
+pygame.quit()
 def get_next_state():
     pass
