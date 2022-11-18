51f54f01c774b75082cb505f32fbcd2d5d9087a4
Colin BERNARD <colin.bernard@ymail.com>
2022-11-18 19:25:59 +0100

    reformated with classes

--- game_of_life.py
+++ game_of_life.py
@@ -1,134 +1,89 @@
 import pygame
-# import random
 import copy
-import game_of_life.params as params
+from game_of_life.params import *
+from game_of_life.game import Game
 
 
-Matrix = list[list[int]]
+class Display:
+    def __init__(self, window_size: tuple, current_game: Game):
+        self._game = current_game
+        self._WINDOW_SIZE: tuple = window_size
+        self._WINDOW_NAME: str = "Game of life"
+        self._SQUARE_X_SIZE: int = self._WINDOW_SIZE[0] // self._game.get_columns_quantity()
+        self._SQUARE_Y_SIZE: int = self._WINDOW_SIZE[1] // self._game.get_lines_quantity()
+        self._frequency: int = 4
+        self._generation: int = 1
+        self._current_state: Matrix = copy.deepcopy(self._game.get_initial_state())
+        self._is_playing: bool = True
+        self._done: bool = False
+        self._init_pygame()
+        self._display()
 
-
-LINES: int = len(params.INITIAL_STATE)
-COLUMNS: int = len(params.INITIAL_STATE[0])
-SQUARE_X_SIZE: int = params.WINDOW_SIZE[0] // COLUMNS
-SQUARE_Y_SIZE: int = params.WINDOW_SIZE[1] // LINES
-
-
-def increase_speed():
-    if params.frequency < 128: params.frequency = params.frequency * 2
-    print(f'Frequency : {params.frequency} Hz')
-
-
-def decrease_speed():
-    if params.frequency > 1: params.frequency = params.frequency // 2
-    print(f'Frequency : {params.frequency} Hz')
-
-
-def main():
-    pygame.init()
-    screen: pygame.surface.Surface = pygame.display.set_mode(params.WINDOW_SIZE)
-    pygame.display.set_caption("Game of life")
-    clock = pygame.time.Clock()
-    font = pygame.font.SysFont("calibri", 50)
-    done: bool = False
-    current_state: Matrix = copy.deepcopy(params.INITIAL_STATE)
-    generation: int = 1
+    def _init_pygame(self):
+        pygame.init()
+        self._screen: pygame.surface.Surface = pygame.display.set_mode(self._WINDOW_SIZE)
+        pygame.display.set_caption(self._WINDOW_NAME)
+        self._clock = pygame.time.Clock()
+        self._font = pygame.font.SysFont("calibri", 50)
     
+    def _display(self):
+        # While the game is not over
+        while not self._done:
 
-    # While the game is not over
-    while not done:
+            # Listen for all events
+            for event in pygame.event.get():
 
-        # Listen for all events
-        for event in pygame.event.get():
-
-            # Quit the infinite loop when the user presses the close button
-            if event.type == pygame.QUIT:
-                done = True
-            
-            if event.type == pygame.KEYDOWN:
-
-                if event.key == pygame.K_UP:
-                    increase_speed()
-                elif event.key == pygame.K_DOWN:
-                    decrease_speed()
-        
-        for y in range(LINES):
-            for x in range(COLUMNS):
-                if current_state[y][x] == 1:
-                    color = (0, 0, 0)
-                else:
-                    color = (255, 255, 255)
+                # Quit the infinite loop when the user presses the close button
+                if event.type == pygame.QUIT:
+                    self._done = True
                 
-                pygame.draw.rect(screen, color, (x * SQUARE_X_SIZE, y * SQUARE_Y_SIZE, SQUARE_X_SIZE, SQUARE_Y_SIZE))
-        
-        text = font.render(f'{generation}', True, (255, 0, 0))
-        screen.blit(text, (0, 0))
+                if event.type == pygame.KEYDOWN:
+
+                    if event.key == pygame.K_UP:
+                        self._increase_speed()
+                    elif event.key == pygame.K_DOWN:
+                        self._decrease_speed()
+                    """elif event.key == pygame.K_SPACE:
+                        self._playpause()"""
+            
+            self._display_current_state()
+            self._current_state = self._game.get_next_state(self._current_state)
+            self._generation += 1
+            self._clock.tick(self._frequency)
+
+        pygame.quit()
+    
+    def _increase_speed(self):
+        if self._frequency < 128: self._frequency = self._frequency * 2
+        print(f'Frequency : {self._frequency} Hz')
+
+    def _decrease_speed(self):
+        if self._frequency > 1: self._frequency = self._frequency // 2
+        print(f'Frequency : {self._frequency} Hz')
+    
+    """def _playpause(self):
+        # à revoir car mettre la fréquence à 0 n'arrête pas la simulation
+        if self._is_playing:
+            self._clock.tick(0)
+            self._is_playing = False
+        else:
+            self._clock.tick(self._frequency)
+            self._is_playing = True"""
+    
+    def _display_current_state(self):
+        for y in range(self._game.get_lines_quantity()):
+                for x in range(self._game.get_columns_quantity()):
+                    if self._current_state[y][x] == 1:
+                        color = (0, 0, 0)
+                    else:
+                        color = (255, 255, 255)
+                    
+                    pygame.draw.rect(self._screen, color, (x * self._SQUARE_X_SIZE, y * self._SQUARE_Y_SIZE, self._SQUARE_X_SIZE, self._SQUARE_Y_SIZE))
+            
+        self._text = self._font.render(f'{self._generation}', True, (255, 0, 0))
+        self._screen.blit(self._text, (0, 0))
         pygame.display.flip()
 
-        current_state = get_next_state(current_state)
-        generation += 1
-        clock.tick(params.frequency)
 
-    pygame.quit()
-
-
-"""def generate_random_coordinates() -> tuple:
-    coordinates: tuple = (random.randint(0, 600), random.randint(0, 600))
-    return coordinates"""
-
-
-def get_next_state(current_state: Matrix) -> Matrix:
-    # print("Getting next_state")
-    next_state: Matrix = create_empty_matrix(LINES, COLUMNS)
-
-    for y in range(LINES):
-            for x in range(COLUMNS):
-                # print(f'Checking cell [{x}][{y}]')
-                if will_the_cell_live(current_state, current_state[y][x], x, y):
-                    next_state[y][x] = 1
-                else:
-                    next_state[y][x] = 0
-    
-    return next_state
-
-
-def create_empty_matrix(lines: int, columns: int) -> Matrix:
-    matrix: Matrix = []
-
-    for y in range(lines):
-        matrix.append([])
-        for x in range(columns):
-            matrix[y].append(0)
-    
-    return matrix
-
-
-def will_the_cell_live(current_state_matrix: Matrix, current_state_of_cell: int, x: int, y: int) -> bool:
-    # print(f'Checking if cell [{x}][{y}] will live')
-    living_cells_around: int = 0
-
-    for surrounding_cell_Y in range(y -1, y + 2):
-        for surrounding_cell_X in range(x - 1, x + 2):
-            if 0 <= surrounding_cell_X <= (COLUMNS - 1) and 0 <= surrounding_cell_Y <= (LINES - 1):
-                # print(f'Checking cell [{surrounding_cell_X}][{surrounding_cell_Y}]')
-                living_cells_around += current_state_matrix[surrounding_cell_Y][surrounding_cell_X]
-            
-    living_cells_around -= current_state_matrix[y][x]
-    # print(f'Living cells around : {living_cells_around}')
-    
-    if current_state_of_cell == 0 and living_cells_around == 3:
-        # print(f'Cell [{x}][{y}] will be born')
-        return True
-    elif current_state_of_cell == 1 and 2 <= living_cells_around <= 3:
-        # print(f'Cell [{x}][{y}] will keep living')
-        return True
-    else:
-        # print(f'Cell [{x}][{y}] will die')
-        return False
-
-
-main()
-
-
-# print(create_empty_matrix(LINES, COLUMNS))
-
-# print(will_the_cell_live(params.INITIAL_STATE, params.INITIAL_STATE[y][x], x, y))
+new_game = Game(INITIAL_STATE)
+Display(WINDOW_SIZE, new_game)
--- game_of_life/game.py
+++ game_of_life/game.py
@@ -1,2 +1,69 @@
-def get_next_state(state):
-    pass
+import sys
+
+PROJECT_PATH = r"D:\DOCUMENTS\Cours ENJMIN\2022-09_Algorithmique et programmation\tp-python-game-of-life\game_of_life"
+
+if PROJECT_PATH not in sys.path:
+    sys.path.append(PROJECT_PATH)
+
+from params import *
+
+
+class Game:
+    def __init__(self, initial_state: Matrix):
+        self._INITIAL_STATE: Matrix = initial_state
+        self._LINES: int = len(self._INITIAL_STATE)
+        self._COLUMNS: int = len(self._INITIAL_STATE[0])
+    
+    def get_initial_state(self) -> Matrix:
+        return self._INITIAL_STATE
+    
+    def get_lines_quantity(self) -> int:
+        return self._LINES
+    
+    def get_columns_quantity(self) -> int:
+        return self._COLUMNS
+    
+    def get_next_state(self, current_state: Matrix) -> Matrix:
+        # print("Getting next_state")
+        self._next_state: Matrix = self._create_empty_matrix(self._LINES, self._COLUMNS)
+
+        for y in range(self._LINES):
+                for x in range(self._COLUMNS):
+                    # print(f'Checking cell [{x}][{y}]')
+                    if self._will_the_cell_live(current_state, current_state[y][x], x, y):
+                        self._next_state[y][x] = 1
+                    else:
+                        self._next_state[y][x] = 0
+        
+        return self._next_state
+    
+    def _create_empty_matrix(self, lines: int, columns: int) -> Matrix:
+        self._empty_matrix: Matrix = []
+
+        for y in range(lines):
+            self._empty_matrix.append([])
+            for x in range(columns):
+                self._empty_matrix[y].append(0)
+        
+        return self._empty_matrix
+    
+    def _will_the_cell_live(self, current_state: Matrix, current_state_of_cell: int, x: int, y: int) -> bool:
+        # print(f'Checking if cell [{x}][{y}] will live')
+        living_cells_around: int = 0
+
+        for surrounding_cell_Y_coord in range(y - 1, y + 2):
+            for surrounding_cell_X_coord in range(x - 1, x + 2):
+                if 0 <= surrounding_cell_X_coord <= (self._COLUMNS - 1) and 0 <= surrounding_cell_Y_coord <= (self._LINES - 1):
+                    # print(f'Checking cell [{surrounding_cell_X}][{surrounding_cell_Y}]')
+                    living_cells_around += current_state[surrounding_cell_Y_coord][surrounding_cell_X_coord]
+                
+        living_cells_around -= current_state[y][x]
+        # print(f'Living cells around : {living_cells_around}')
+        
+        if (
+            (current_state_of_cell == 0 and living_cells_around == 3) or
+            (current_state_of_cell == 1 and 2 <= living_cells_around <= 3)
+        ):
+            return True
+        else:
+            return False
