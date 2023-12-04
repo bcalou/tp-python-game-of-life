30338be734e54542b800cef2251525a4eca1e96a
TheoThenault <theo.thenault@gmail.com>
2023-12-04 16:18:00 +0100

    Game Of Life fonctionnel

--- __main__.py
+++ __main__.py
@@ -1,26 +1,28 @@
 import pygame
+from game_of_life.get_next_state import get_next_state
+from game_of_life.get_neighbours_count import get_neighbours_count
+import random
+
+WIDTH = 2200
+HEIGHT = 1300
+pixel_size = 4
 
 def main():
     print("Hello world")
     pygame.init()
-    screen = pygame.display.set_mode((1120, 600))
+    screen = pygame.display.set_mode((WIDTH, HEIGHT))
 
     clock = pygame.time.Clock()
 
     done = False
 
-    initial_state: list[list[int]] = [
-        [0, 0, 0, 0, 0, 1, 0, 0, 0, 0],
-        [0, 0, 0, 0, 0, 1, 1, 1, 0, 0],
-        [0, 0, 0, 1, 0, 0, 1, 0, 0, 1],
-        [0, 0, 0, 0, 0, 0, 0, 1, 0, 0],
-        [0, 0, 0, 1, 0, 0, 0, 0, 0, 0],
-        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-        [0, 0, 0, 0, 0, 0, 1, 0, 0, 0],
-        [0, 0, 0, 0, 0, 0, 0, 1, 0, 0],
-        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
-    ]
+    def rlint(size: int) -> list[int]:
+        return [random.randint(0, 1) for _ in range(size)]
+
+    state: list[list[int]] = [rlint(WIDTH//pixel_size) for _ in range(HEIGHT//pixel_size)]
+    # for y in range(0, 3):
+    #         print(f"{get_neighbours_count(state, 0, y)} {get_neighbours_count(state, 1, y)} {get_neighbours_count(state, 2, y)}")
+
     # While the game is not over
     while not done:
 
@@ -30,29 +32,38 @@ def main():
             if event.type == pygame.QUIT:
                 done = True
             
-            # Effacer l'écran
-            screen.fill((0, 0, 0))
+        # debug
+        # print("\n\n\n\n\n\n")
+        # for y in range(len(state)):
+        #     for x in range(len(state[0])):    
+        #         print(state[y][x], end="")
+        #     print("")
 
-            # Déssine l'etat initial_state
-            print_state(screen, initial_state)
-            
-            # Applique les déssins sur l'écran
-            pygame.display.flip()
 
+        # Effacer l'écran
+        screen.fill((0, 0, 0))
+
+        # Déssine l'etat initial_state
+        print_state(screen, state)
+
+        # Applique les déssins sur l'écran
+        pygame.display.flip()
+
+        # Calcule l'etat suivant
+        state = get_next_state(state)
+        
         # print("Update !")
-        clock.tick(30)
+        clock.tick(120)
 
     pygame.quit()
 
-pixel_size = 25
+
 def print_state(screen, state: list[list[int]]) -> None:
     for y, row in enumerate(state):
         for x, cell in enumerate(row):
-            if cell == 1:
-                pygame.draw.rect(screen, (255, 255, 255), (x*pixel_size, y*pixel_size, x+pixel_size, y+pixel_size))
-            else:
-                pygame.draw.rect(screen, (50, 50, 50), (x*pixel_size, y*pixel_size, x+pixel_size, y+pixel_size))
-
+            color: int = cell*220 + 35
+            pygame.draw.rect(screen, (color, color, color), 
+                            (x*pixel_size, y*pixel_size, pixel_size-1, pixel_size-1))
 
 
 main()
---
+++ game_of_life/get_neighbours_count.py
@@ -0,0 +1,18 @@
+def get_neighbours_count(state: list[list[int]], x: int, y: int) -> int:
+    count: int = 0
+
+    heigh: int = len(state)
+    width: int = len(state[0])
+
+    x_coords: list[int] = [x-1, x,  x+1]
+    y_coords: list[int] = [y-1, y, y+1]
+
+    for neighbour_x in x_coords:
+        for neighbour_y in y_coords:
+            if neighbour_x is not x or neighbour_y is not y:
+                if (neighbour_x >= 0 and neighbour_x < width
+                and neighbour_y >= 0 and neighbour_y < heigh):
+                    # print(f"{x}/{y} : {neighbour_x}/{neighbour_y} ({state[neighbour_x][neighbour_y]})")
+                    count += state[neighbour_y][neighbour_x]
+
+    return count
