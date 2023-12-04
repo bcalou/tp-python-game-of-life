70d7f83d8d7cc23abe43eebb56fa8bc1eb0c0c36
bterral <baptiste.terral.auditeur@lecnam.net>
2023-12-04 14:43:28 +0100

    Fin étape 2, 3, 4 (et 5)

--- game_of_life/global_variables.py
+++ game_of_life/global_variables.py
@@ -1,2 +1,8 @@
-WINDOW_WIDTH: int = 1000;
-WINDOW_HEIGHT: int = 1000;
+WINDOW_WIDTH: int = 1000
+WINDOW_HEIGHT: int = 800
+
+CELL_SIZE: int = 10
+
+# Colors
+BLACK: tuple[int, int, int] = (0, 0, 0)
+WHITE: tuple[int, int, int] = (255, 255, 255)
