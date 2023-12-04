1165e4d2c1e897d0471ad09a4c818427d5754a8f
bterral <baptiste.terral.auditeur@lecnam.net>
2023-12-04 11:46:08 +0100

    Fin étape 1

--- __main__.py
+++ __main__.py
@@ -3,7 +3,29 @@ import pygame
 pygame.init()
 
 def main():
-    print("Hello world")
+    screen = pygame.display.set_mode((1000, 1000))
+    clock = pygame.time.Clock()
+    done = False
 
+    # While the game is not over
+    while not done:
+        # Listen for all events
+        for event in pygame.event.get():
+            # Quit the infinite loop when the user presses the close button
+            if event.type == pygame.QUIT:
+                done = True
+
+        # Update the screen
+        # fill screen with white fileld squares and black borders
+        for i in range(0, 1000, 100):
+            for j in range(0, 1000, 100):
+                pygame.draw.rect(screen, (255, 255, 255), (i, j, 100, 100), 0)
+                pygame.draw.rect(screen, (0, 0, 0), (i, j, 100, 100), 1)
+        
+        pygame.display.flip()
+        print("Update !")
+        clock.tick(1)
+    
+    pygame.quit()
 
 main()
