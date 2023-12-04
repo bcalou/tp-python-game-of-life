6ee50e89b4b43b8632a8346740e0131e5a6406be
bterral <baptiste.terral.auditeur@lecnam.net>
2023-12-04 16:11:48 +0100

    Now respects pycodestyle

--- game_of_life/game_of_life.py
+++ game_of_life/game_of_life.py
@@ -1,6 +1,7 @@
 import pygame
 import game_of_life.global_variables as gv
 
+
 class Game_of_life:
     state_matrix: list[list[int]] = []
 
@@ -20,23 +21,26 @@ class Game_of_life:
                 count += current_state[i][j]
         count -= current_state[x][y]
         return count
-        
-    def get_next_state(self, current_state: list[list[int]]) -> list[list[int]]:
+
+    def get_next_state(self, current_state: list[list[int]]) -> list[
+            list[int]]:
         """
         Returns the next state of the game of life.
         """
-        next_state = [[0 for _ in range(len(current_state[0]))] for _ in range(len(current_state))]
+        next_state = [[0 for _ in range(len(current_state[0]))]
+                      for _ in range(len(current_state))]
         for i in range(len(current_state)):
             for j in range(len(current_state[i])):
                 neighbors = self.count_neighbors(i, j, current_state)
-                if current_state[i][j] == 1 and (neighbors < 2 or neighbors > 3):
+                if current_state[i][j] == 1 and (
+                        neighbors < 2 or neighbors > 3):
                     next_state[i][j] = 0
                 elif current_state[i][j] == 0 and neighbors == 3:
                     next_state[i][j] = 1
                 else:
                     next_state[i][j] = current_state[i][j]
         return next_state
-    
+
     def draw(self, screen: pygame.Surface):
         """
         Draws the current state of the game of life.
@@ -44,4 +48,7 @@ class Game_of_life:
         for i in range(len(self.state_matrix)):
             for j in range(len(self.state_matrix[i])):
                 if self.state_matrix[i][j] == 1:
-                    pygame.draw.rect(screen, gv.BLACK, (j * gv.CELL_SIZE, i * gv.CELL_SIZE, gv.CELL_SIZE, gv.CELL_SIZE))
+                    pygame.draw.rect(
+                        screen, gv.BLACK, (
+                            j * gv.CELL_SIZE, i * gv.CELL_SIZE, gv.CELL_SIZE,
+                            gv.CELL_SIZE))
