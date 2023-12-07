d8c53ea38f448c9b215d9ee7485d38ed31dcb39d
TheoThenault <theo.thenault@gmail.com>
2023-12-07 15:13:36 +0100

    Support custom rules

--- game_of_life/game_of_life.py
+++ game_of_life/game_of_life.py
@@ -18,29 +18,41 @@ class GameOfLife:
         """
         self.__board: Board = Board(initial_state)
 
+    def game_rules(self, cell_state: int, neighbour_count: int) -> int:
+        """
+        Defines the rule of the game of life.
+
+        Returns the cell state for the next iteration
+
+        A children class with different rules 
+        must overwrite this function
+        """
+        # A cell stays/becomes alive if it has 3 neighbours
+        if neighbour_count == 3:
+            return ALIVE
+        # A cell stays alive/dead if it has 2 neighbours
+        elif neighbour_count == 2:
+            return cell_state
+        # With <2 or >3 neighbour, a cell dies
+        return DEAD
+
     def next_state(self) -> None:
         """
-        Updates the internal state of the game according to Conway's
-        original rules of Game of life
+        Updates the internal state of the game according to the rules defined
+        in method `GameOfLife.game_rules()`
         """
-        next_state: list[list[int]] = []
+        new_state: list[list[int]] = []
         for y in range(self.get_height()):
             new_row: list[int] = []
             for x in range(self.get_width()):
                 neighbour_count: int = self.__board.get_neighbours_count(x, y)
+                cell_state: int = self.get_cell(x, y)
 
-                # A cell stays/becomes alive if it has 3 neighbours
-                if neighbour_count == 3:
-                    new_row.append(ALIVE)
-                # A cell stays alive/dead if it has 2 neighbours
-                elif neighbour_count == 2:
-                    new_row.append(self.__board.get_cell(x, y))
-                # With <2 or >3 neighbour, a cell dies
-                else:
-                    new_row.append(DEAD)
-            next_state.append(new_row)
+                new_row.append(self.game_rules(cell_state, neighbour_count))
 
-        self.__board.set_board(next_state)
+            new_state.append(new_row)
+
+        self.__board.set_board(new_state)
 
     def get_width(self) -> int:
         """
