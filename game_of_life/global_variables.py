5ffb513ba08ede7eca701a17435dfd5444de8565
bterral <baptiste.terral.auditeur@lecnam.net>
2023-12-07 11:39:37 +0100

    defined Matrix type

--- game_of_life/global_variables.py
+++ game_of_life/global_variables.py
@@ -7,14 +7,17 @@ CELL_SIZE: int = 10
 BLACK: tuple[int, int, int] = (0, 0, 0)
 WHITE: tuple[int, int, int] = (255, 255, 255)
 
+# Types
+Matrix = list[list[int]]
+
 # Initial states
 
-INITIAL_STATE: list[list[int]] = [
+INITIAL_STATE: Matrix = [
         [0, 1, 0],
         [0, 1, 0],
         [0, 1, 0]
     ]
-INITIAL_STATE2: list[list[int]] = [
+INITIAL_STATE2: Matrix = [
     [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
         0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
         0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
