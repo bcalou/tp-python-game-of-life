2d2d0940c8dfab40a120d7e563760bc16f353198
bterral <baptiste.terral.auditeur@lecnam.net>
2023-12-08 14:18:23 +0100

    Reorganized functions by importance order

--- game_of_life/game_of_life.py
+++ game_of_life/game_of_life.py
@@ -11,30 +11,6 @@ class Game_of_life:
         """
         self.state_matrix = initial_state
 
-    def __count_neighbors(self, x: int, y: int,
-                          current_state: gv.Matrix) -> int:
-        """
-        Counts the number of neighbors of a cell.
-        """
-        # Initialize a counter to 0
-        count = 0
-
-        # Loop over the cell's neighborhood
-        for i in range(max(0, x - 1), min(x + 2, len(current_state))):
-            for j in range(max(0, y - 1), min(y + 2, len(current_state[0]))):
-                # Add the cell's value to the counter
-                # A cell with a value of 1 is alive and a cell with a value
-                # of 0 is dead.
-                count += current_state[i][j]
-
-        # Subtract cell's own value from the counter because it was also
-        # included in the neighborhood, but we want to count only the
-        # neighbors, not the cell itself.
-        count -= current_state[x][y]
-
-        # Return the count
-        return count
-
     def get_next_state(self, current_state: gv.Matrix) -> gv.Matrix:
         """
         Returns the next state of the game of life.
@@ -64,3 +40,27 @@ class Game_of_life:
                         screen, gv.BLACK, (
                             j * gv.CELL_SIZE, i * gv.CELL_SIZE, gv.CELL_SIZE,
                             gv.CELL_SIZE))
+
+    def __count_neighbors(self, x: int, y: int,
+                          current_state: gv.Matrix) -> int:
+        """
+        Counts the number of neighbors of a cell.
+        """
+        # Initialize a counter to 0
+        count = 0
+
+        # Loop over the cell's neighborhood
+        for i in range(max(0, x - 1), min(x + 2, len(current_state))):
+            for j in range(max(0, y - 1), min(y + 2, len(current_state[0]))):
+                # Add the cell's value to the counter
+                # A cell with a value of 1 is alive and a cell with a value
+                # of 0 is dead.
+                count += current_state[i][j]
+
+        # Subtract cell's own value from the counter because it was also
+        # included in the neighborhood, but we want to count only the
+        # neighbors, not the cell itself.
+        count -= current_state[x][y]
+
+        # Return the count
+        return count
