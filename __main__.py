1d77ed32f199fc370c0d9e5f8682b05a6a451332
TheoThenault <theo.thenault@gmail.com>
2023-12-08 10:47:30 +0100

    Suppréssion ancien code

--- __main__.py
+++ __main__.py
@@ -1,6 +1,9 @@
 import pygame
-from game_of_life.get_next_state import get_next_state
-from game_of_life.default import GLIDER_GUN
+from pygame import Surface
+from game_of_life.game_of_life import GameOfLife
+from game_of_life.default import GLIDER_GUN, HIGHLIFE_REPLICATOR
+from game_of_life.highlife import HighLife
+from game_of_life.board import generate_random_state
 
 
 WIDTH = 980
@@ -8,21 +11,23 @@ HEIGHT = 980
 PIXEL_SIZE = 20
 
 
-def main():
-    print("Hello world")
+def main_with_classes():
     pygame.init()
-    screen = pygame.display.set_mode((WIDTH, HEIGHT))
+    screen: Surface = pygame.display.set_mode((WIDTH, HEIGHT))
 
     clock = pygame.time.Clock()
 
     done = False
 
-    state: list[list[int]] = GLIDER_GUN
+    print(f"SIMULATION SIZE: {(WIDTH//PIXEL_SIZE, HEIGHT//PIXEL_SIZE)}")
 
-    # for y in range(0, 3):
-    #         print(f"{get_neighbours_count(state, 0, y)} \
-    #                 {get_neighbours_count(state, 1, y)} \
-    #                 {get_neighbours_count(state, 2, y)}")
+    # game_of_life: GameOfLife = GameOfLife(
+    #     generate_random_state(WIDTH//PIXEL_SIZE, HEIGHT//PIXEL_SIZE)
+    # )
+
+    # game_of_life: GameOfLife = GameOfLife(GLIDER_GUN)
+
+    game_of_life: HighLife = HighLife(HIGHLIFE_REPLICATOR)
 
     # While the game is not over
     while not done:
@@ -33,39 +38,22 @@ def main():
             if event.type == pygame.QUIT:
                 done = True
 
-        # debug
-        # print("\n\n\n\n\n\n")
-        # for y in range(len(state)):
-        #     for x in range(len(state[0])):
-        #         print(state[y][x], end="")
-        #     print("")
-
         # Effacer l'écran
         screen.fill((0, 0, 0))
 
         # Déssine l'etat initial_state
-        print_state(screen, state)
+        game_of_life.print_state(screen, PIXEL_SIZE)
 
         # Applique les déssins sur l'écran
         pygame.display.flip()
 
         # Calcule l'etat suivant
-        state = get_next_state(state)
+        game_of_life.next_state()
 
         # print("Update !")
-        clock.tick(120)
+        clock.tick(30)
 
     pygame.quit()
 
 
-def print_state(screen, state: list[list[int]]) -> None:
-    """Print cell to the screen according to the state"""
-    for y, row in enumerate(state):
-        for x, cell in enumerate(row):
-            color: int = cell*220 + 35
-            pygame.draw.rect(screen, (color, color, color), (
-                             x*PIXEL_SIZE, y*PIXEL_SIZE,
-                             PIXEL_SIZE-1, PIXEL_SIZE-1))
-
-
-main()
+main_with_classes()
